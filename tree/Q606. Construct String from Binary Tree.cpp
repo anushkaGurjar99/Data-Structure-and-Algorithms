07aa142e40e79757aab4baf39bb6b15332919106
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-02-10 18:48:48 +0530

    Create Q606. Construct String from Binary Tree.cpp

---
+++ tree/Q606. Construct String from Binary Tree.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author : Anuska Gurjar
+ * Date   : Februaruy 2020
+ * flags    : -std=c++14
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+*/
+
+// Problem Statement: https://leetcode.com/problems/construct-string-from-binary-tree/
+
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        if(!t)
+            return "";
+        if(!t->right && !t->left)
+            return to_string(t->val);
+        if(!t->right)
+            return to_string(t->val) + "(" + tree2str(t->left) + ")";
+        return to_string(t->val) + "(" + tree2str(t->left) + ")(" + tree2str(t->right) + ")";
+    }
+};
