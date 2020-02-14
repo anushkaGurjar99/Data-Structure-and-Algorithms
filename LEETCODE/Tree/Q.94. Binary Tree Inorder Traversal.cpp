8800df5bc002efbc91142acfa2cb27b94292ad24
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-02-14 12:42:12 +0530

    Create Q.94. Binary Tree Inorder Traversal.cpp

---
+++ LEETCODE/Tree/Q.94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,50 @@
+/*
+ * Author : Anuska Gurjar
+ * Date   : Februaruy 2020
+ * flags    : -std=c++14
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+*/
+
+// Problem Statement: https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root){
+        vector<int> values;
+        if(!root) 
+            return values;
+        
+        TreeNode* temp = root;
+        stack<TreeNode*> nodes;
+        
+        while(true){
+            while(temp){
+                nodes.push(temp);
+                temp = temp->left;
+            }
+            
+            if(nodes.empty()) 
+                break;
+            
+            temp = nodes.top();
+            nodes.pop();
+            values.push_back(temp->val);
+            temp = temp->right;
+        }
+        return values;
+    }
+};
+
