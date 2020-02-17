c9769feb2533627e490816f604d3a31ae3564f4c
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-02-17 14:35:03 +0530

    Update Q543. Diameter of Binary Tree.cpp

--- LEETCODE/Tree/Q543. Diameter of Binary Tree.cpp
+++ LEETCODE/Tree/Q543. Diameter of Binary Tree.cpp
@@ -38,3 +38,23 @@ public:
         return max(left, right) + 1;
     }
 };
+
+
+/*
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        getDiameter(root, diameter);
+        return diameter;
+    }
+    int getDiameter(TreeNode* node, int& diameter){
+        if(!node)
+            return 0;
+        int left = getDiameter(node->left, diameter);
+        int right = getDiameter(node->right, diameter);
+        diameter = (left + right) > diameter ? (left + right) : diameter;
+        return 1 + max(left, right);
+    }
+};
+*/
