85ccf4024478ead601dd1adbe94a648a657a6918
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-02-17 10:42:58 +0530

    Create Q145. Binary Tree Postorder Traversal.cpp

---
+++ LEETCODE/Tree/Q145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author : Anuska Gurjar
+ * Date   : Februaruy 2020
+ * flags    : -std=c++14
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+*/
+
+// Problem Statement: https://leetcode.com/problems/binary-tree-postorder-traversal
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        
+        if(!root)
+            return result;
+        
+        stack<TreeNode*> nodes;
+        nodes.push(root);
+        
+        while(!nodes.empty()){
+            TreeNode *current = nodes.top();
+            nodes.pop();
+            if(current->left)
+                nodes.push(current->left);
+            if(current->right)
+                nodes.push(current->right);
+            result.insert(result.begin(), current->val);
+        }
+        return result;
+    }
+};
