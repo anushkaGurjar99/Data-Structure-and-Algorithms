3ba83ea28c625de99afecc1bc314f4f2a69327f9
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-03 10:59:36 +0530

    Update Q116. Populating Next Right Pointers in Each Node.cpp

--- LEETCODE/Tree/Q116. Populating Next Right Pointers in Each Node.cpp
+++ LEETCODE/Tree/Q116. Populating Next Right Pointers in Each Node.cpp
@@ -29,18 +29,18 @@ public:
 
 class Solution {
 public:
-    Node* connect(Node* root) {
-        if (!root) {
-            return NULL;
+    Node* connect(Node* root){
+        if(!root)
+            return nullptr;
+        
+        if(root->left){                 // no need to check right child as its given node will have either 0 or 2 children
+            root->left->next = root->right;
+            if(root->next)
+                root->right->next = root->next->left;
         }
-        if (root -> left) {
-            root -> left -> next = root -> right;
-            if (root -> next) {
-                root -> right -> next = root -> next -> left;
-            }
-            connect(root -> left);
-            connect(root -> right);
-        }
+        
+        connect(root->left);
+        connect(root->right);
         return root;
     }
 };
