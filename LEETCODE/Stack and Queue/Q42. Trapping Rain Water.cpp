64d919285aad57ef0f21bf493353c077d302571c
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-07 11:06:17 +0530

    Update Q42. Trapping Rain Water.cpp

--- LEETCODE/Stack and Queue/Q42. Trapping Rain Water.cpp
+++ LEETCODE/Stack and Queue/Q42. Trapping Rain Water.cpp
@@ -54,3 +54,54 @@ public:
         return water;
     }
 };
+
+// ********************************* Two Pointer Approach *********************************
+class Solution {
+public:
+    int trap(vector<int>& height){
+        
+        if(height.size() <= 1)
+            return 0;
+        
+        int waterCollected = 0;
+        
+        int start = 0;
+        int end = height.size() - 1;
+        int head = height[start];
+        int tail = height[end];
+        
+        while(start < end){
+            
+            if(height[start] < height[end]){
+                start++;
+                if(height[start] < head)
+                    waterCollected += head - height[start];
+                else
+                    head = height[start];
+            }
+            else{
+                end--;
+                if(height[end] < tail)
+                    waterCollected += tail - height[end];
+                else
+                    tail = height[end];
+            }
+        }
+        
+        return waterCollected;
+    }
+};
+
+/*
+Two pointer approach ('start' and 'end')
+    
+    move the pointer which is lesser 
+    After moving, 
+    if the element is less than 'head' or 'tail'
+        add the water(diffenence) into container
+    else
+        make the element as 'head' or 'tail'
+    this shows that how much the maximum water we can collect
+    terminate the loop when start == end
+    
+*/
