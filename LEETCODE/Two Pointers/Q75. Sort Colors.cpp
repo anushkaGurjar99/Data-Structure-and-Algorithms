f83cb6d0fe84421ccc124e93318485a59956eddd
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-14 16:20:39 +0530

    Update Q75. Sort Colors.cpp

--- LEETCODE/Two Pointers/Q75. Sort Colors.cpp
+++ LEETCODE/Two Pointers/Q75. Sort Colors.cpp
@@ -12,30 +12,30 @@ using namespace std;
 // see leetcode notes for explanation
 class Solution {
     public:
-    void sortColors(vector<int>& nums) 
-    {
-        int swap = 0;
+    void sortColors(vector<int>& nums) {
+        
         int low = 0;
         int mid = 0;
-        int high = nums.size() - 1;
-    
+        int high = nums.size()-1;
+        int swap;
+        
         while(mid <= high){
-            if(nums[mid] == 0){
-                swap = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = swap;
+            if(nums[mid] == 1){             // incr the mid
+                mid++;                     
+            }
+            else if(nums[mid] == 0){        // swap mid with low
+                swap = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = swap;
                 low++;
-                mid++;
+                mid++;                     // because we know before mid we have sorted elements
             }
-            else if(nums[mid] == 1){
-                mid++;
+            else{                          // swap mid with high
+                swap = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = swap;
+                high--;                    // mid can't be ince as swapped element has to be settle
             }
-            else if(nums[mid] == 2){
-                swap = nums[high];
-                nums[high] = nums[mid];
-                nums[mid] = swap;
-                high--;
-            }
-        }
+        } 
     }
 };
