99950d1c9b3891ccd1af8f6ee0b23b7aac595d39
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-18 14:57:22 +0530

    Create Q200. Number of Islands.cpp

---
+++ LEETCODE/BFS/Q200. Number of Islands.cpp
@@ -0,0 +1,88 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/number-of-islands
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid){
+        
+        if(grid.size() == 0)
+            return 0;
+        
+        int numberOfIsland = 0;
+        
+        for(int row = 0; row < grid.size(); row++){
+            
+            for(int col = 0; col < grid[row].size(); col++){
+                
+                if(grid[row][col] == '1'){
+                    
+                    numberOfIsland++;
+                    queue<pair<int,int>> waterArea;
+                    waterArea.push({row,col});
+                    grid[row][col] = '2';                   // to remove cycles
+                    
+                    
+                    // removing the area connected by water current point
+                    while(!waterArea.empty()){
+                        pair<int,int> current = waterArea.front();
+                
+                        waterArea.pop();
+                        
+                        int r = current.first;
+                        int c = current.second;
+                        
+                        // remove bottom area
+                        r++;
+                        while(r < grid.size() && grid[r][c] == '1'){
+                            grid[r][c] = '2';
+                            waterArea.push({r,c});
+                            r++;
+                        }
+                        
+                        r = current.first; // restore row intex
+                        
+                        // remove left area
+                        c--;
+                        while(c >= 0 && grid[r][c] == '1'){
+                            grid[r][c] = '2';
+                            waterArea.push({r,c});
+                            c--;
+                        }
+                        
+                        c = current.second; // restore col index
+                        
+                        // remove top area
+                        r--;
+                        while(r >= 0 && grid[r][c] == '1'){
+                            grid[r][c] = '2';
+                            waterArea.push({r,c});
+                            r--;
+                        }
+                        
+                        r = current.first; // restore row intex
+                        
+                        // remove right area
+                        c++;
+                        while(c < grid[r].size() && grid[r][c] == '1'){
+                            grid[r][c] = '2';
+                            waterArea.push({r,c});
+                            c++;
+                        }
+                    }
+                        
+                }   
+            }
+        }
+        
+        return numberOfIsland;
+    }
+};
