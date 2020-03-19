dbaf1b38a8f062028ec7fb05d1e4b77a4f7f78a8
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-19 11:56:56 +0530

    Update Q207. Course Schedule.cpp

--- LEETCODE/BFS/Q207. Course Schedule.cpp
+++ LEETCODE/BFS/Q207. Course Schedule.cpp
@@ -10,6 +10,53 @@ using namespace std;
 
 // Problem Statement: https://leetcode.com/problems/course-schedule
 
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites){
+        
+        unordered_map<int, vector<int>> sequence;
+        vector<int> degree(numCourses, 0);
+        
+        // store all dependencies and degree of each course
+        for(auto& coursePair: prerequisites){
+            int independent = coursePair[1];
+            int dependent = coursePair[0];
+            
+            sequence[independent].push_back(dependent);
+            degree[dependent]++;
+        }
+        
+        // store all independent courses, as they come first in topological sorting
+        queue<int> order;
+        for(int i = 0; i < numCourses; i++){
+            if(degree[i] == 0)
+                order.push(i);
+        }
+        
+        int coursesObtained = 0;
+        
+        
+        while(!order.empty()){
+            coursesObtained++;
+            
+            int curr = order.front();
+            order.pop();
+            
+            for(auto& next: sequence[curr]){
+                degree[next]--;                    // decrement the dependencies;
+                if(degree[next] == 0)              // keep storing independent courses
+                    order.push(next);
+            }
+            
+        }
+        
+        return coursesObtained == numCourses;
+    }
+};
+
+
+
+
 // TIME LIMIT EXCEED, pass all test cases except large one
 class Solution {
 public:
