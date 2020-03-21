88d227dbf5f795abf0cd30a888dc7d9d0c3f13df
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-21 09:28:26 +0530

    Create Q206. Reverse Linked List.cpp

---
+++ LEETCODE/Linked List/Q206. Reverse Linked List.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : February 2020
+ * flags    : -std=c++14
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+// Problem Statement: https://leetcode.com/problems/reverse-linked-list
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        ListNode* currNode = head;
+        ListNode* prevNode = nullptr;
+        
+        while(currNode){
+            ListNode* nextNode = currNode->next;
+            currNode->next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        
+        // currNode is null while prevNode is pointing to last node which is our head
+        head = prevNode;
+        
+        return head;
+    }
+};
