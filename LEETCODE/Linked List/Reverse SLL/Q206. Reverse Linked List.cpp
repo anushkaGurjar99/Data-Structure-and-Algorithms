773b88df50a4b2c639032487fbc87717130bbf29
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-22 14:54:46 +0530

    Update Q206. Reverse Linked List.cpp

--- LEETCODE/Linked List/Reverse SLL/Q206. Reverse Linked List.cpp
+++ LEETCODE/Linked List/Reverse SLL/Q206. Reverse Linked List.cpp
@@ -39,3 +39,18 @@ public:
         return head;
     }
 };
+
+//****************************************** Recursive **************************************************
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){   
+        if(!head || !head->next)
+            return head;
+        
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+};
