148e666df1a1e3f33f424fce11dad681e3cb068a
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-27 15:15:12 +0530

    Create Q49. Group Anagrams.cpp

---
+++ LEETCODE/String/Q49. Group Anagrams.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/group-anagrams
+
+class Solution{
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs){
+        
+        vector<vector<string>> anagrams;
+        unordered_map<string, vector<string>> m;
+        
+        for(string curr : strs){
+            string temp = stringSort(curr);
+            m[temp].push_back(curr);
+        }
+        
+        for(auto key : m)
+            anagrams.push_back(key.second);
+
+        return anagrams;
+    }
+        
+private:
+    string stringSort(string s){
+        
+        int counter[26] = {0};
+        
+        for(char c : s)
+            counter[c - 'a']++;
+        
+        string t;
+        
+        for(int i = 0; i < 26; i++)
+            t += string(counter[i], i + 'a');
+        
+        return t;
+    }
+};
