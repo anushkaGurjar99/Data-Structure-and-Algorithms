fc42d6c0517df987fd7c1b3e119478b8433ac246
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-28 15:08:59 +0530

    Create QTop K Frequent Elements.cpp

---
+++ LEETCODE/Heap/QTop K Frequent Elements.cpp
@@ -0,0 +1,46 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/top-k-frequent-elements/
+
+typedef pair<int, int> P;
+class Solution {
+public:
+	vector<int> topKFrequent(vector<int>& nums, int k) {
+		
+        unordered_map<int, int> m;
+		
+        for (int x : nums) 	
+            m[x]++;
+                
+		priority_queue<P, vector<P>, greater<P>> q;      
+        // this is syntax of MIN Heap, by default it is the MAX heap
+        
+		for(auto x : m){
+			if (q.size() < k){
+                q.push({x.second, x.first});
+            }
+			else{
+				if(q.top().first < x.second){
+					q.pop();
+					q.push({x.second, x.first});
+				}
+			}
+		}
+		
+        vector<int> result;
+		while (!q.empty()){
+			result.push_back(q.top().second);
+			q.pop();
+		}
+        
+		return result;
+	}
+};
+
