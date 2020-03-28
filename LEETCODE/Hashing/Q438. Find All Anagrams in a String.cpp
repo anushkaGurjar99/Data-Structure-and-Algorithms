ca9873d180cbfeebdff34f4358ee135edc5c1b71
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-28 22:25:48 +0530

    Create Q438. Find All Anagrams in a String.cpp

---
+++ LEETCODE/Hashing/Q438. Find All Anagrams in a String.cpp
@@ -0,0 +1,74 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+
+// TIME LIMIT EXCEED
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+          
+        vector<int> result;
+        unordered_map<char, vector<int>> m;
+        unordered_map<char, int> c;
+         
+        
+        for(int i = 0; i < p.size(); i++)
+            c[p[i]]++;
+        
+        
+        unordered_map<char, int> save = c;
+        int length = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            
+            auto itr = c.find(s[i]);
+            
+            if(itr != c.end()){
+                if(itr->second > 0){
+                    length++;
+                    c[s[i]]--;
+                }
+                
+                m[s[i]].push_back(i);
+                
+                if(length == p.size()){
+                    
+                    int minn = INT_MAX; 
+                    for(auto itr: m)
+                        minn = min(minn, itr.second[itr.second.size() - save[itr.first]] );
+                    
+                    if(minn + p.size() - 1 == i)
+                        result.push_back(minn);
+                    
+                    m.clear();
+                    c = save;
+                    length = 0;
+                    i = minn;                       // search from this pos again
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+/*
+Take two hash:
+  one for its frequence 
+  second for the indexes where it has occured
+  
+as soon as we encountered all characters of demanded string, 
+  we find the least recent occured element from indexes array
+
+if the least recently occured elemnt + len of demanded string is == current char 
+this mean that we have covered all the elements (so we push the least recently occured element in result array)
+*/
+
