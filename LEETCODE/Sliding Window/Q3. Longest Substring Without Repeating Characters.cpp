9c78c967bf4659079cac40846d7f81b2719388fc
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-03-29 14:34:45 +0530

    Update Q3. Longest Substring Without Repeating Characters.cpp

--- LEETCODE/Sliding Window/Q3. Longest Substring Without Repeating Characters.cpp
+++ LEETCODE/Sliding Window/Q3. Longest Substring Without Repeating Characters.cpp
@@ -42,3 +42,40 @@ START and END denoting the begin and end of our current window
 keep incr END in every iteration, check if s[i] already exist in CURRENT window then move START to
 that occurance
 */
+
+
+// *************************************************************************************************************
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string A){
+        
+        unordered_map<char, int> m;
+    
+        int result = 0;
+        int start = 0;
+
+        for(int i = 0; i < A.size(); i++){
+
+            auto itr = m.find(A[i]);
+            if(itr != m.end() && itr->second >= start)
+                start = itr->second + 1;
+
+            result = max(result, i - start + 1);
+            m[A[i]] = i;
+        }
+
+        return result;
+    }
+};
+
+/*
+
+(memory efficient: storing only recent occurance)
+
+START and i denoting the begin and end of our current window
+
+keep incr i in every iteration, check if s[i] already exist in CURRENT window then move START to
+that occurance
+*/
+
