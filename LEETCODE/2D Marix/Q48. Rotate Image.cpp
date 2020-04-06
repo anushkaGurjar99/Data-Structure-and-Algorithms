03160f8bf629328417131a47b55ade91058b842a
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-06 16:04:44 +0530

    Update Q48. Rotate Image.cpp

--- LEETCODE/2D Marix/Q48. Rotate Image.cpp
+++ LEETCODE/2D Marix/Q48. Rotate Image.cpp
@@ -33,3 +33,27 @@ public:
 // swap all 4 values in a sequence
 // Ex: 1,2,3,4 => 2,3,4,1 (3 adjecent swapping)
 
+
+// *************************************** Easier Approch ***************************************************
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix){
+        
+        int size = matrix.size();
+        
+        // Transpose the Matrix
+        for(int i = 0; i < size; i++){
+            for(int j = i + 1; j < size; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+        
+        // Reverse each row
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size/2; j++)
+                swap(matrix[i][j], matrix[i][size-j-1]);
+        }
+        
+    }
+};
+
