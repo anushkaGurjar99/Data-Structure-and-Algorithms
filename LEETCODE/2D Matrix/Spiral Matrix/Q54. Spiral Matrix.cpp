6705360edbef0c18a1061e8808d7e5cb37e0310b
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-07 17:10:14 +0530

    Update Q54. Spiral Matrix.cpp

--- LEETCODE/2D Matrix/Spiral Matrix/Q54. Spiral Matrix.cpp
+++ LEETCODE/2D Matrix/Spiral Matrix/Q54. Spiral Matrix.cpp
@@ -55,10 +55,91 @@ public:
 };
 
 /*
-Check if the corrosponding ROW or COL is in Range(using the IF cond.)
-If it is then visit the cells
+Check if the corrosponding ROW or COL is in Range(using the IF cond.) (FOR ALL DIRECTIONS)
 TOP : visit all cells
 RIGHT : visit all cells (except TOP)
 BOTTOM : visit all cells (except TOP)
 LEFT: visit all cells (except TOP and BOTTOM)
 */
+
+// ******************************************* Using Switch and Enum ****************************************
+
+class Solution {
+public:
+    
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        
+        if(matrix.size() < 1)
+            return {};
+        
+        enum dir{RIGHT, DOWN, LEFT, UP};
+        
+        vector<int> res;
+        int limit = matrix.size()*matrix[0].size();
+
+        int rowBegin = 0;
+        int rowEnd = matrix.size();
+        int colBegin = 0;
+        int colEnd = matrix[0].size();
+
+        int r = 0;
+        int c = 0;
+        dir direction = RIGHT;
+
+        while(res.size() < limit){
+            
+            switch(direction){
+                
+                case RIGHT:{
+                    while(c < colEnd){
+                        res.push_back(matrix[r][c]); 
+                        c++; 
+                    }
+                    r++;
+                    c--;
+                    direction = DOWN;
+                }
+                break;
+                    
+                case DOWN:{
+                    while(r < rowEnd){
+                        res.push_back(matrix[r][c]); 
+                        r++; 
+                    }
+                    r--;
+                    c--;
+                    direction = LEFT;
+                }
+                break;
+
+                case LEFT:{
+                    while(c >= colBegin){
+                        res.push_back(matrix[r][c]); 
+                        c--; 
+                    }
+                    r--;
+                    c++;
+                    direction = UP;
+                }
+                break;
+                    
+                case UP:{
+                    while(r > rowBegin){
+                        res.push_back(matrix[r][c]);
+                        r--;
+                    }
+                    r++;
+                    c++;
+                    rowBegin++;
+                    colBegin++;
+                    rowEnd--;
+                    colEnd--;
+                    direction = RIGHT;
+                }
+                break;
+            }
+        }
+        return res;
+    }
+};
+
