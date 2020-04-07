5d5b463fa490c03993d6720445ba46cab64850cc
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-07 14:33:10 +0530

    Create Q240. Search a 2D Matrix II.cpp

---
+++ LEETCODE/2D Matrix/Q240. Search a 2D Matrix II.cpp
@@ -0,0 +1,42 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : April 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target){
+        
+        if(matrix.size() < 1)
+            return false;
+        
+        int row = 0;
+        int col = matrix[0].size() - 1;
+        
+        while(row < matrix.size() && col >= 0){
+            if(matrix[row][col] == target)
+                return true;
+            
+            if(matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        }
+        
+        return false;
+    }
+};
+
+/*
+Start from Top-Right corner,
+  Return true if target found 
+  If the cell value is Greater than target -> Move left
+  If the cell value is Less than target -> Move bottom
+Exit if you run out of cells and return false;
+*/
