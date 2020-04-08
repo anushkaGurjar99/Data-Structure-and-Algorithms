88d7c75f9c85b817681763574cfb30244800dc79
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-08 14:43:57 +0530

    Create Q520. Detect Capital.cpp

---
+++ LEETCODE/String/Q520. Detect Capital.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/detect-capital/
+
+class Solution {
+public:
+    bool detectCapitalUse(string word){
+        
+        if(word.size() < 2)
+            return true;
+        
+        bool result;
+        
+        if(islower(word[0]))
+            result = isAllSmall(word);
+        else
+            result = islower(word[1]) ? isAllSmall(word) : isAllCapital(word);
+            
+        return result;
+    }
+    
+    bool isAllSmall(string s){
+        for(int i = 1; i < s.size(); i++){
+            if(s[i] < 'a' || s[i] > 'z')
+                return false;
+        }
+        return true;
+    }
+    
+    bool isAllCapital(string s){
+        for(int i = 1; i < s.size(); i++){
+            if(s[i] < 'A' || s[i] > 'Z')
+                return false;
+        }
+        return true;
+    }
+};
