3c70825faec90aeee6eb5f29bd477e6c5ab2f837
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-08 14:02:46 +0530

    Create Q344. Reverse String.cpp

---
+++ LEETCODE/String/Q344. Reverse String.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/reverse-string/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s){
+        reverse(0, s.size() - 1, s);
+    }
+    
+    void reverse(int start, int end, vector<char>& str){
+        if(start < end){
+            char temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            
+            reverse(start + 1, end - 1, str);
+        }
+    }
+};
+//Recursive
