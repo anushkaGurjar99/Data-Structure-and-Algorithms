c326e891438c42538c8820888d7dc1b7b0d5ce43
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-09 13:42:23 +0530

    Update Longest Common Prefix.cpp

--- Interviewbit/Strings/Longest Common Prefix.cpp
+++ Interviewbit/Strings/Longest Common Prefix.cpp
@@ -9,6 +9,32 @@ using namespace std;
 
 // Problem Statement: https://www.interviewbit.com/problems/longest-common-prefix/
 
+string Solution::longestCommonPrefix(vector<string> &A){
+    if(A.size() == 0)
+        return "";
+        
+    if(A.size() == 1)
+        return A[0];
+        
+    string result = "";
+        
+    for(int i = 0; i < A[0].size(); i++){
+        char ch = A[0][i];
+        for(int j = 1; j < A.size(); j++){
+            if(ch != A[j][i])
+                return result;
+        }
+        result += ch;
+    }
+    return result;
+}
+
+/*
+Vertical SCAN
+*/
+
+// ********************************************** Compare each Pair of String **********************************************
+
 string commonPrefix(string one, string two){
     string result = "";
     
