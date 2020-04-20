45d201a8b12328d9064527ea25b022296c7e0d5c
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-20 16:30:32 +0530

    Update Integer To Roman.cpp

--- Interviewbit/Strings/Integer To Roman.cpp
+++ Interviewbit/Strings/Integer To Roman.cpp
@@ -23,3 +23,69 @@ string Solution::intToRoman(int num){
     
     return result;
 }
+
+// ************************************************ Another approach ************************************************
+
+string Solution::intToRoman(int num){
+    unordered_map<int, string> m;
+    m[1] = "I";
+    m[5] = "V";
+    m[10] = "X";
+    m[50] = "L";
+    m[100] = "C";
+    m[500] = "D";
+    m[1000] = "M";
+        
+    string result = "";
+        
+    int zeros = 1;
+    int start = 1;
+    int end = 5;
+        
+    while(num != 0){
+    int curr = num % 10;
+        string roman = "";
+        
+        if(curr == 9){
+            roman = m[start] + m[start * 10];
+        }
+        else if(curr == 4){
+            roman = m[start] + m[end];
+        }
+        else{
+            curr *= zeros;
+            while(curr > 0){
+                if(curr < end){
+                    roman += m[start];
+                    curr -= start;
+                }
+                else{
+                    roman += m[end];
+                    curr -= end;
+                }
+            }
+        }   
+        result = roman + result;
+        start *= 10, end *= 10, zeros *= 10;
+        num /= 10;
+    }   
+    return result;
+}
+
+
+/*
+Approach:
+    Convert the number from back
+    
+    for ex: if the number is 2571
+            the we have to generate roman of 1, 70, 500, 2000 then merge the sequence
+    
+    'start' and 'end' denoting the roman number available for particular number
+    say for 63 we have the start = X and end = L
+    
+    if it is special case -> encode directly
+    else convert accordingly
+    
+    each time we update 'number of zeros', 'start', 'end'
+*/
+
