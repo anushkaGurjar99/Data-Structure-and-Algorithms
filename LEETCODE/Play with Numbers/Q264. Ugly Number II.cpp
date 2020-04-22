3ad1934d502102c498ffda77124f1906f9e4e770
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-04-22 23:23:54 +0530

    Update Q264. Ugly Number II.cpp

--- LEETCODE/Play with Numbers/Q264. Ugly Number II.cpp
+++ LEETCODE/Play with Numbers/Q264. Ugly Number II.cpp
@@ -14,17 +14,17 @@ public:
     int nthUglyNumber(int n) {
         vector<int> ugly(n, 1);
         
-        int c2 = 2, c3 = 3, c5 = 5;
+        int a = 2, b = 3, c = 5;
         int i2 = 0, i3 = 0, i5 = 0;
         
         for (int i=1; i<n; ++i) {
-            int last = ugly[i] = min(c2, min(c3, c5));
-            while(c2 <= last)
-                c2 = 2 * ugly[++i2];
-            while(c3 <= last) 
-                c3 = 3 * ugly[++i3];
-            while(c5 <= last) 
-                c5 = 5 * ugly[++i5];
+            int last = ugly[i] = min(a, min(b, c));
+            while(a <= last)
+                a = 2 * ugly[++i2];
+            while(b <= last) 
+                b = 3 * ugly[++i3];
+            while(c <= last) 
+                c = 5 * ugly[++i5];
             
         }
         return ugly[n-1];
