2a3e1453651ce39457d2b06e5327a699e9a692c6
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-05-03 16:25:08 +0530

    Update Q1347. Minimum Number of Steps to Make Two Strings Anagram.cpp

--- LEETCODE/String/Q1347. Minimum Number of Steps to Make Two Strings Anagram.cpp
+++ LEETCODE/String/Q1347. Minimum Number of Steps to Make Two Strings Anagram.cpp
@@ -12,6 +12,53 @@ using namespace std;
 class Solution {
 public:
     int minSteps(string s, string t) {
+        unordered_map<char,int> one;
+        unordered_map<char,int> two;
+        
+        for(auto ch: s)
+            one[ch]++;
+        for(auto ch: t)
+            two[ch]++;
+        
+        int diff = 0;
+        
+        for(auto i: one){
+            int freq = i.second;
+            char ch = i.first;
+            if(two.find(ch) != two.end())
+                diff += freq > two[ch] ? (freq - two[ch]) : 0;
+            else
+                diff += freq;
+        }
+        return diff;
+    }
+};
+
+/*
+ string s            string t
+"leetcode"          "practice"
+
+o = 1                   0
+l = 1                   0
+e = 3                   1
+t = 1                   1
+c = 1                   2
+d = 1                   0
+                        i = 1
+                        p = 1
+                        r = 1
+                        a = 1
+    
+    occurances to convert string t into string s
+    1 + 1 + 2 + 0 + (-1) + 1
+*/   
+    
+    
+// ******************************************** Using Single Map *******************************************************
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
         unordered_map<char,int> m;
         
         for(auto ch: s)
@@ -29,31 +76,3 @@ public:
         return diff;
     }
 };
-
-
-/*
-
- string s            string t
-"leetcode"          "practice"
-
-o = 1                   0
-l = 1                   0
-e = 3                   1
-t = 1                   1
-c = 1                   2
-d = 1                   0
-                        i = 1
-                        p = 1
-                        r = 1
-                        a = 1
-    
-    occurances to convert string t into string s
-    1 + 1 + 2 + 0 + (-1) + 1
-
-*/   
-    
-    
-    
-    
-    
-    
