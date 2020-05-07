3d5db016eb5b65303b3aca0cd4b4e65d8a5c4581
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-05-07 16:10:41 +0530

    Create Q154. Find Minimum in Rotated Sorted Array II.cpp

---
+++ LEETCODE/Binary Search/Q154. Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,45 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : May 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums){
+        
+        if(nums.size() == 0)
+            return INT_MIN;
+        
+        if(nums.size() == 1)
+            return nums[0];
+        
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+        
+        while(left <= right && nums[left] == nums[right])        
+            right--;
+        
+        while(left <= right){
+            mid = left + (right - left) / 2;
+            
+            if(nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        
+        return min(nums[mid], nums[left]);
+    }
+};
+
+/*
+Remove all duplicates of pivot from last then do usual BS.
+Applying this will not generate confusion for SIDE Selection
+*/
