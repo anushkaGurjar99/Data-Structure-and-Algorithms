f8ce7cd2da2c93d7f9fa039a14e381decee69444
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-05-08 17:46:41 +0530

    Update Q784. Letter Case Permutation.cpp

--- LEETCODE/Backtracking/Q784. Letter Case Permutation.cpp
+++ LEETCODE/Backtracking/Q784. Letter Case Permutation.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 // Problem Statement: https://leetcode.com/problems/letter-case-permutation/
 
+// DFS
 class Solution{
 public:
     vector<string> letterCasePermutation(string S){
@@ -54,4 +55,66 @@ FLOW =>     amr
             
 Toggle logic: 97(a) - 65(A) is equal to 32.
 */
+
+// ************************************************* BFS ***********************************************************
+
+class Solution{
+public:
+    vector<string> letterCasePermutation(string S){
+        
+        vector<string> result;
+        queue<string> q;
+        string levelMarker = " ";
+        
+        for(int i = 0; i < S.size(); i++){
+            
+            if(isalpha(S[i])){
+                if(q.empty()){
+                    q.push(S);
+                    S[i] ^= 32;
+                    q.push(S);
+                    S[i] ^= 32;
+                    q.push(levelMarker);
+                }
+                else{
+                    while(q.front() != levelMarker){
+                        string curr = q.front();  
+                        q.push(curr);
+                        curr[i] ^= 32;
+                        q.push(curr);
+                        q.pop();
+                    }
+                    q.pop();                            // pop levelMarker
+                    q.push(levelMarker);
+                }
+            }
+        }
+        
+        while(!q.empty()){
+            if(q.front() != levelMarker)
+                result.push_back(q.front());
+            q.pop();
+        }      
+        if(result.size() == 0)
+            result.push_back(S);
+        
+        return result;
+    }
+};
+
+/*
+BFS appraoch:
+
+Image there is a binary tree, we start from the dummy root, and the left child node means choice
+1(append a lower case), and right child node is choice 2(upper case). Hence if String here is
+"ab", we have a tree looks like this.
+                            
+                         /     \
+                        a       A
+                       /  \    / \
+                      ab  aB  Ab  AB
+
+Obviously our goal here will be printing out all leaf nodes.
+*/
+
 
