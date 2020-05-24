a4df8a452b88719bb604e31e13cc41c50dd946ea
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-05-24 14:59:06 +0530

    Create Q62. Unique Paths.cpp

---
+++ LEETCODE/Dynamic Programming/Q62. Unique Paths.cpp
@@ -0,0 +1,56 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : May 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/unique-paths/
+
+class Solution{
+public:
+    int uniquePaths(int m, int n){
+        
+        if(m == 0)
+            return 0;
+        
+        vector<int> path(m, 1);
+        
+        for(int i = 1; i < n; i++)                          // row 1 already discovered
+            getUniquePathOfCurrentRow(path);
+        
+        return path[m - 1];        
+    }
+    
+    void getUniquePathOfCurrentRow(vector<int>& path){
+        for(int i = 1; i < path.size(); i++)
+            path[i] = path[i] + path[i - 1];
+    }    
+};
+
+/*  
+Intution:
+
+Lets take an example of (3 * 3) matrix
+we know that for each cell we have 2 ways to reach out:
+    - top cell path
+    - left cell path
+
+    1   -   -
+    -   -   -
+    -   -   -           (initially), becuase there is only one path to reach first cell
+    
+    
+    To get unique path for a cell sum up : (left path + top path)
+
+   (1)  1   1
+    1   2   3
+    1   3  (6)
+  
+
+Note: we can create a 2D matrix too.
+
+-- Similar to climibing stairs.
+*/
