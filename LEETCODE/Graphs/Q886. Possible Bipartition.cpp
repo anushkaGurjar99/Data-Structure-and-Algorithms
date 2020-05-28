cb03751486249308fa5396146f9bb08ef7735b4e
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-05-28 15:16:18 +0530

    Create Q886. Possible Bipartition.cpp

---
+++ LEETCODE/Graphs/Q886. Possible Bipartition.cpp
@@ -0,0 +1,53 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : March 2020
+ * flags    : -std=c++14
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/possible-bipartition/
+
+class Solution{
+public:
+    bool possibleBipartition(int N, vector<vector<int>> &edges) {
+        vector<vector<int>> adj(N + 1); 
+        
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        vector<int> color(N + 1, 0); 
+        vector<bool> explored(N + 1, false); 
+        queue<int> q;
+        
+        for(int i = 1; i <= N; ++i){
+            if(color[i] == 0){
+                color[i] = 1;
+                q.push(i);
+                
+                while(!q.empty()){
+                    int u = q.front();
+                    q.pop();
+                    
+                    if(explored[u])
+                        continue;
+                    explored[u] = true;
+                    
+                    for(auto v: adj[u]){
+                        if (color[v] == color[u])
+                           return false;
+                        color[v] = (color[u] == 1) ? 2 : 1;
+                        q.push(v);
+                    }
+                }
+            }
+        }      
+        return true;
+    }
+};
