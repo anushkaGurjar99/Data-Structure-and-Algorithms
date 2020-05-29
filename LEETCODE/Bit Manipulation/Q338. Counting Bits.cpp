a181d5354891a6f4eade5cae8cc6972dff9c8caa
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-05-29 18:49:22 +0530

    Update Q338. Counting Bits.cpp

--- LEETCODE/Bit Manipulation/Q338. Counting Bits.cpp
+++ LEETCODE/Bit Manipulation/Q338. Counting Bits.cpp
@@ -52,3 +52,43 @@ public:
 Basically each iteration produces (2^k = sizeOfPartialArray) binary bits.
 */
 
+// ************************************************* DP Solution ***********************************************************
+
+class Solution{
+public:
+    vector<int> countBits(int num){
+        
+        if(num == 0)
+            return {0};
+        
+        vector<int> result(num + 1);
+        result[0] = 0;
+        result[1] = 1;
+        int offset = 2;    
+        
+        for(int i = 2; i <= num; i++){
+            result[i] = result[i - offset] + 1;
+            if(i + 1 == offset * 2)
+                offset *= 2;
+        }                
+        return result;
+    }
+};
+
+/*
+Pattern to get the general function.
+    dp[0] = 0;
+    dp[1] = dp[1-1] + 1;
+
+    dp[2] = dp[2-2] + 1;
+    dp[3] = dp[3-2] +1;
+
+    dp[4] = dp[4-4] + 1;
+    dp[5] = dp[5-4] + 1;
+    dp[6] = dp[6-4] + 1;
+    dp[7] = dp[7-4] + 1;
+
+    dp[8] = dp[8-8] + 1;
+*/
+
+// https://leetcode.com/problems/counting-bits/discuss/79557/How-we-handle-this-question-on-interview-Thinking-process-%2B-DP-solution
