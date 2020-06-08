c133c9e7e8f97b0b8b4cc1b69dea785d2c6be1bd
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-06-08 15:33:09 +0530

    Update Longest Arithmetic Progression.cpp

--- Interviewbit/Dynamic Programming/Longest Arithmetic Progression.cpp
+++ Interviewbit/Dynamic Programming/Longest Arithmetic Progression.cpp
@@ -14,6 +14,45 @@ int Solution::solve(const vector<int> &A){
     if(A.size() <= 2)
         return A.size();
     
+    int n = A.size();
+    int result = 0;
+    vector<unordered_map<int, int>> dp(n);
+        
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            int diff = A[i] - A[j];
+            
+            dp[i][diff] = dp[j].count(diff) > 0 ? dp[j][diff] + 1 : 2;
+            
+            result = max(result, dp[i][diff]);
+        }
+    }
+    return result;
+}
+
+/*
+How to keep track of the length as well as the difference? 
+We can use a hashmap, whose key is the difference and value is the length. 
+
+The optimization: 
+    for two elements A[i] and A[j] where j < i, A[i] - A[j] is a critical condition. 
+    
+    If the hashmap has key 'diff', 
+        There exist an subsequence ending at index j, with difference diff and length dp[j][diff]. 
+    And we just add the length by 1. 
+    
+    If hashmap does not have the key diff, 
+        then those two elements can form a 2-length arithmetic subsequence.
+*/
+
+
+// ******************************************************* Using Set *******************************************************
+
+int Solution::solve(const vector<int> &A){
+    
+    if(A.size() <= 2)
+        return A.size();
+    
     unordered_multiset<int> um;
     unordered_set<int> us;
     int result = 0;
