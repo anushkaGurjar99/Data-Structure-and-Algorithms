bb7fb5cb0a9bfd4ed5eed7dd8a3b19f6b774c31c
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-07-15 17:35:54 +0530

    Update Q509. Fibonacci Number.cpp

--- LEETCODE/Recursion/Q509. Fibonacci Number.cpp
+++ LEETCODE/Recursion/Q509. Fibonacci Number.cpp
@@ -32,3 +32,24 @@ public:
 Base case:
   return last calculated element when target is reached.
 */
+
+
+// ******************************************************* More elegant ****************************************************************
+
+class Solution{
+public:
+    int fib(int N){
+        if(N <= 1)
+            return N;
+        
+        return fib(N - 1) + fib(N - 2);
+    }
+};
+
+/*
+    Base case:
+        N = 0, return 0
+        N = 1, return 1
+ 
+If not base case then return sum of prev 2 elements till base case reached.
+*/
