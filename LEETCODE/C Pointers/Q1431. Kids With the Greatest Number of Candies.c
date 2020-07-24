57fbbc41f29fdb57f72706040aff7fd57e572382
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-07-24 15:49:27 +0530

    Update Q1431. Kids With the Greatest Number of Candies.c

--- LEETCODE/C Pointers/Q1431. Kids With the Greatest Number of Candies.c
+++ LEETCODE/C Pointers/Q1431. Kids With the Greatest Number of Candies.c
@@ -4,19 +4,13 @@
  * flags  : C
 */
 
-
-#include<bits/stdc++.h>
-using namespace std;
-
 // Problem Statement: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize){
     
     bool* result = (bool*) malloc(candiesSize * sizeof(bool));
-    
+    *returnSize = candiesSize;
+
     int maxCandies = 0;
     
     for(int i = 0; i < candiesSize; i++)
@@ -24,8 +18,6 @@ bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* retu
     
     for(int i = 0; i < candiesSize; i++)
         result[i] = (candies[i] + extraCandies >= maxCandies) ? true : false;
-    
-    *returnSize = candiesSize;
     
     return result;
 }
