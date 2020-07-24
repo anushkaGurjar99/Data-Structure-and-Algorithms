da19b18e41e3b35497c7e596e9ee6c0747e6ceaf
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-07-24 15:48:02 +0530

    Create Q1389. Create Target Array in the Given Order.c

---
+++ LEETCODE/C Pointers/Q1389. Create Target Array in the Given Order.c
@@ -0,0 +1,33 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : July 2020
+ * flags  : C
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+int* createTargetArray(int* nums, int numsSize, int* index, int indexSize, int* returnSize){
+    
+    *returnSize = numsSize;
+    
+    int* result = (int*) malloc(numsSize * sizeof(int));
+    
+    for(int i = 0; i < numsSize; i++){
+        
+        int pos = i;
+        
+        // free up the cell by shifting...
+        while(pos > index[i]){
+            result[pos] = result[pos - 1];
+            pos--;
+        }
+        
+        result[index[i]] = nums[i];
+    }
+    
+    return result;
+}
