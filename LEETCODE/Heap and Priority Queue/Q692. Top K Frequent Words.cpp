8f6d2d10af7f9a97562ea032bda4349d07c647a6
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-07-30 20:06:38 +0530

    Update Q692. Top K Frequent Words.cpp

--- LEETCODE/Heap and Priority Queue/Q692. Top K Frequent Words.cpp
+++ LEETCODE/Heap and Priority Queue/Q692. Top K Frequent Words.cpp
@@ -14,38 +14,55 @@ class Solution{
 
 private:
     struct comparator{
-      bool operator()(const P& lhs, const P& rhs){
-        if(lhs.first == rhs.first){
-            // freq is same, lexographically greater first ( kinda max heap ) : opposite sign used
-            return lhs.second < rhs.second;
+        bool operator()(const P& one, const P& two){
+            if(one.first == two.first)
+                return one.second < two.second;
+            return one.first > two.first;
         }
-        // min heap : less freq first : opposite sign to be used
-        return lhs.first > rhs.first;
-    }
-};
-    
+    };
 public:
     vector<string> topKFrequent(vector<string>& words, int k){
         
-        unordered_map<string, int> m;
-        for(auto word: words)
-            m[word]++;
-        
-        priority_queue<P, vector<P>, comparator> pq;        // MIN heap
-        for(auto itr: m){
-            pq.push({itr.second, itr.first});
-            if(pq.size() > k)
-                pq.pop();
-        }
-        // case: [a, aa, aaa], k = 1 => output should be 'a' not 'aaa'
-        
         vector<string> result;
+        unordered_map<string, int> m;
+        fillMap(m, words);
+        
+        priority_queue<P, vector<P>, comparator> pq;
+        fillPQ(m, pq, k);
+        
         while(!pq.empty()){
             result.push_back(pq.top().second);
             pq.pop();
         }
         
         reverse(result.begin(), result.end());
+        
         return result;
     }
+    
+    void fillMap(unordered_map<string, int>& m, vector<string>& words){
+        for(auto word: words)
+            m[word]++;
+    }
+    
+    void fillPQ(unordered_map<string, int>& m, priority_queue<P, vector<P>, comparator>& pq, int k){
+        for(auto itr: m){
+            pq.push({itr.second, itr.first});
+            if(pq.size() > k)
+                pq.pop();   
+        }
+    }
 };
+
+/*
+    We know that String supports bool operations such has < > == and so on..
+    
+    step1:  calculate word frequencies
+    step2:  create a min Heap, store K strings into heap that has HIGHEST frequency.  (that means if PQ size will exceed the K -> we will pop the least Freq element.
+            If any collisions occurs in frequency, then check lexographic order.
+    step3:  store the PQ elements into result
+    step4:  reverse the result array since it was MIN heap so it would have returned incr order of freq.
+*/
+
+
+
