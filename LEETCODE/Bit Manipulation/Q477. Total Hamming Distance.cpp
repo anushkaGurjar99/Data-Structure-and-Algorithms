e2e54b4b86f0e414f5d51225573a4edd49b40a89
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-07-31 15:38:56 +0530

    Create Q477. Total Hamming Distance.cpp

---
+++ LEETCODE/Bit Manipulation/Q477. Total Hamming Distance.cpp
@@ -0,0 +1,38 @@
+/*
+ * Author : Anushka Gurjar
+ * Date   : May 2020
+ * flags    : -std=c++14
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem Statement: https://leetcode.com/problems/total-hamming-distance/
+
+class Solution{
+public:
+    int totalHammingDistance(vector<int>& nums){
+        
+        if(nums.size() <= 0) 
+            return 0;
+        
+        int res = 0;
+        
+        for(int i = 0; i < 32; i++){
+            int setCount = 0;
+            
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[j] & (1 << i))
+                    setCount++;
+            }
+            
+            int zeroCount = nums.size() - setCount;
+            
+            res += setCount * zeroCount;
+        }
+        
+        return res;
+    }
+};
+
+// https://leetcode.com/problems/total-hamming-distance/discuss/720409/Python-Bit-Manipulation-Explanation-with-Diagram
