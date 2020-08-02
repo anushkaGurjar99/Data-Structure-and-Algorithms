e4090ab420eb196bd88cd9bac8ad551f207619bf
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-08-02 15:30:06 +0530

    Update Q198. House Robber.cpp

--- LEETCODE/Dynamic Programming/House Robber/Q198. House Robber.cpp
+++ LEETCODE/Dynamic Programming/House Robber/Q198. House Robber.cpp
@@ -13,24 +13,23 @@ class Solution{
 public:
     int rob(vector<int>& nums){
         
-        if(nums.size() < 1)
+        int size = nums.size();
+        
+        if(size == 0)
             return 0;
         
-        if(nums.size() == 1)
-            return nums[0];
-        
-        if(nums.size() == 2)
-            return max(nums[0], nums[1]);
+        if(size <= 2)
+            return (size == 1) ? nums[0] : max(nums[0], nums[1]);
         
         vector<int> dp(nums.size(), 0);
         
         dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        dp[1] = max(nums[1], nums[0]);
         
-        for(int i = 2; i < nums.size(); i++)
+        for(int i = 2; i < size; i++)
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         
-        return dp[nums.size() - 1];
+        return max(dp[size - 1], dp[size - 2]);
     }
 };
 
