dd7f1d9438d28651d5509dddaf330342c849cf1a
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-08-09 16:41:28 +0530

    Update Rotated Sorted Array Search.cpp

--- Interviewbit/Binary Search/Rotated Sorted Array Search.cpp
+++ Interviewbit/Binary Search/Rotated Sorted Array Search.cpp
@@ -9,6 +9,51 @@ using namespace std;
 
 // Problem Statement: https://www.interviewbit.com/problems/rotated-sorted-array-search/
 
+int Solution::search(const vector<int> &A, int B){
+    int left = 0;
+    int right = A.size() - 1;
+    
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        
+        if(A[mid] == B)
+            return mid;
+            
+        if(A[left] < A[mid]){
+            if(B >= A[left] && B < A[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        else{
+            if(B > A[mid] && B <= A[right])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+/*
+    target = 10;
+    
+    80   90   100  10   20   30   40   50   60   70 
+    0    1     2   3    4    5    6     7   8    9
+                    
+    left  =  0      0
+    right =  9      3
+    mid   =  4      1....and so on.
+    
+basically check if mid is same as target
+if not then,
+    check which part of array is sorted left-to-mid or mid-to-right
+    check target in sorted part if in range else search unsorted part
+*/
+
+// ********************************************************************************************************************************************
+
 int Solution::search(const vector<int> &nums, int target) {
 
     int l = 0;
