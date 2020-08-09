6598f8d8492fa1e86abf0ca1503410bb9d0823e3
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-08-09 15:37:12 +0530

    Update Sorted Insert Position.cpp

--- Interviewbit/Binary Search/Sorted Insert Position.cpp
+++ Interviewbit/Binary Search/Sorted Insert Position.cpp
@@ -15,25 +15,26 @@ int Solution::searchInsert(vector<int> &A, int B){
         return 0;
     
     int left = 0;
-    int right = A.size();
+    int right = A.size() - 1;
     
-    while(left < right){
+    while(left <= right){
         int mid = left + (right - left) / 2;
-        if(A[mid] == B){
-            left = mid;
-            break;
-        }
+        
+        if(A[mid] == B)
+            return mid;
         
         if(A[mid] < B)
             left = mid + 1;
         else
-            right = mid;
+            right = mid -  1;
     }
     
     return left;
 }
 
+
 /*
+consider the tip, (if you are not sure that will element would be in range or not then use <=)
 Search the target value into array
   if found -> reurn that index
   else -> return last scanned position
