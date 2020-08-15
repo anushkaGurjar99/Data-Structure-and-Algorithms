1b89f7026058aa77cd08ce0eabec82e73ca11b18
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-08-15 18:21:03 +0530

    Update Q295. Find Median from Data Stream.cpp

--- LEETCODE/Heap and Priority Queue/Q295. Find Median from Data Stream.cpp
+++ LEETCODE/Heap and Priority Queue/Q295. Find Median from Data Stream.cpp
@@ -9,6 +9,90 @@ using namespace std;
 
 // Problem Statement: https://leetcode.com/problems/find-median-from-data-stream/
 
+class MedianFinder{
+private:
+    priority_queue<double, vector<double>, greater<double>> right;
+    priority_queue<double> left;
+        
+public:
+    MedianFinder(){}
+    
+    void addNum(int num){
+        
+        int lSize = left.size();
+        int rSize = right.size();
+        
+        if(lSize == 0){
+            left.push(num);
+        }
+        
+        else if(lSize == rSize){
+            if(right.top() >= num){
+                left.push(num);
+            }
+            else{
+                left.push(right.top());
+                right.pop();
+                right.push(num);
+            }
+        }
+        
+        else if(left.top() <= num){
+            right.push(num);
+        }
+        
+        else{
+            right.push(left.top());
+            left.pop();
+            left.push(num);
+        }
+    }
+    
+    double findMedian(){
+        int size = left.size() + right.size();
+        double res = (size % 2 == 0) ? (left.top() + right.top()) / 2 : left.top();
+        return res;
+    }
+};
+
+/*
+Appraoch:
+    divide stream into two parts where,
+    all elements of Left range are smaller than all elements of Right range.
+    
+    Left range:     MAX heap
+    Right range:    MIN heap
+
+find median cases
+    Even case: LR.size() == RR.size()
+    Odd case : LR.size() + 1 == RR.size()
+
+Add number cases - 
+    case1:
+        Left range is empty, so we push at left side
+        
+    case2:
+        Length of Left range == Length of Right range
+            If num is less than or equals to right.top()
+                we push it into left range
+            else
+                we push one element to right range into left range, pop it from right
+                push num into right range
+                
+    case3:
+        (Length of left range is greater than Right range)
+        If left.top() is smaller or equals to the num
+            we push number into right range
+            
+    case4:
+        else of case 3
+        push left.top() into right range, pop it from left 
+        push num into left
+
+*/
+
+// **********************************************************************************************************************************************************
+
 class MedianFinder {
 private:
 
