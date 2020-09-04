e3d3b3fa0889bd3ccd08a73c331f60f651dd52bc
anushkaGurjar <anushka.mcs19.du@gmail.com>
2020-09-04 17:50:32 +0530

    Update Hotel Bookings Possible.cpp

--- Interviewbit/Array/Hotel Bookings Possible.cpp
+++ Interviewbit/Array/Hotel Bookings Possible.cpp
@@ -19,3 +19,30 @@ bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K){
     }
     return true;
 }
+
+// *********************************************************************************************************************************************************************
+
+bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K){
+    
+    if(K >= arrive.size())
+        return 1;
+    
+    sort(arrive.begin(), arrive.end());
+    sort(depart.begin(), depart.end());
+    
+    for(int i = 0; i < arrive.size() - K; i++){
+        if(arrive[i + K] < depart[i])
+            return false;
+    }
+    
+    return true;
+}
+/*
+        1   2   3   4   5
+        2   2   2   2   2
+
+Check how many overlaps are present in one particular range
+If the number of overlaps are greater than the number of rooms, 
+we can say that we have less rooms to accommodate guests.
+*/
+
